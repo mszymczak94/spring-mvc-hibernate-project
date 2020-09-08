2555fbb90f8782bec9bed2b9e5d1d7d715853ffd
Michał Szymczak <mszymczak_94@wp.pl>
2020-09-08 16:49:43 +0200

    Create CustomerDAO, return customer list in controller, adjust jsp, add css styles

--- src/com/szymczak/springdemo/controller/CustomerController.java
+++ src/com/szymczak/springdemo/controller/CustomerController.java
@@ -1,15 +1,23 @@
 package com.szymczak.springdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.szymczak.springdemo.dao.CustomerDAO;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		theModel.addAttribute("customers", customerDAO.getCustomers());
+		
 		return "list-customers";
 	}
 
---
+++ src/com/szymczak/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,34 @@
+package com.szymczak.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.szymczak.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = session.createQuery("from Customer", Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
+	}
+
+}
